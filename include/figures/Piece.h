e9c31514880ce7167a5d127721b323a3a8845d9e
ytrewqleo <leo.vokra@gmail.com>
2026-03-02 14:32:06 +0300

    implement class Piece

--- include/figures/Piece.h
+++ include/figures/Piece.h
@@ -1,8 +1,22 @@
-//
-// Created by admin on 27.02.2026.
-//
-
 #ifndef PROJECT_FIGURES_H
+enum class Color {
+    White,
+    Black
+};
+class Piece {
+protected:
+    int _row;
+    int _col;
+    Color _color;
+    bool _isMoved = false;
+public:
+    Piece(int row, int col, Color color = Color::White);
+    constexpr int getRow() const;
+    constexpr int getCol() const;
+    constexpr Color getColor() const;
+    constexpr virtual bool canMove(int new_row, int new_col) const = 0;
+    virtual ~Piece() = default;
+};
 #define PROJECT_FIGURES_H
 
 #endif //PROJECT_FIGURES_H
--- src/figures/Piece.cpp
+++ src/figures/Piece.cpp
@@ -1,3 +1,16 @@
-//
-// Created by admin on 27.02.2026.
-//
+#include "Piece.h"
+
+Piece::Piece(int row, int col, Color color): _row(row), _col(col), _color(color){}
+
+constexpr int Piece::getRow() const {
+    return _row;
+}
+
+constexpr int Piece::getCol() const {
+    return _col;
+}
+
+constexpr Color Piece::getColor() const {
+    return _color;
+}
+

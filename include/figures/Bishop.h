174b2e9e3ef2977b5ee7c8d1d630da63fd0d4846
ytrewqleo <leo.vokra@gmail.com>
2026-03-03 16:45:00 +0300

    rewrote Bishop class, add allfigures.h which now contain std::variant of PieceVariant

--- include/figures/Bishop.h
+++ include/figures/Bishop.h
@@ -5,11 +5,12 @@
 #ifndef PROJECT_BISHOP_H
 #define PROJECT_BISHOP_H
 #include "Piece.h"
+#include "Board.h"
 
 class Bishop : public Piece{
 public:
     Bishop(int row, int col, Color color = Color::White);
-    constexpr bool canMove(int new_row, int new_col) const override;
+    constexpr bool canMove(int new_row, int new_col, const Board& board) const override;
     ~Bishop() override = default;
 };
 
--- src/figures/Bishop.cpp
+++ src/figures/Bishop.cpp
@@ -2,10 +2,29 @@
 
 Bishop::Bishop(int row, int col, Color color): Piece(row, col, color) {}
 
-constexpr bool Bishop::canMove(int new_row, int new_col) const {
-    int difRow = std::abs(new_row - _row);
-    int difCol = std::abs(new_col - _col);
-    return (difRow == difCol);
+constexpr bool Bishop::canMove(int new_row, int new_col, const Board& board) const {
+    int difRow = new_row - _row;
+    int difCol = new_col - _col;
+    if (std::abs(difCol) != std::abs(difRow) || (difRow == 0 && difCol == 0))
+        return false;
+
+    int stepRow = (difRow > 0) ? 1 : -1;
+    int stepCol = (difCol > 0) ? 1 : -1;
+    int curRow = _row + stepRow;
+    int curCol = _col + stepCol;
+
+    while (curRow != new_row || curCol != new_col) {
+        if (board.isOccupied(curRow, curCol))
+            return false;
+        curRow += stepRow;
+        curCol += stepCol;
+    }
+
+    auto& enemy = board.getPiece(new_row, new_col);
+    if (std::holds_alternative<Empty>(enemy))
+        return true;
+    Color enemyColor = std::visit([](auto& el){return el.getColor();}, enemy);
+    return getColor() != enemyColor;
 }
 
 

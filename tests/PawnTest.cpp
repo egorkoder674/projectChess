247db0d933379cd061528b1d2248e48462e7b281
ytrewqleo <leo.vokra@gmail.com>
2026-03-11 14:38:11 +0300

    made PawnTest.cpp for testing of Pawn

---
+++ tests/PawnTest.cpp
@@ -0,0 +1,73 @@
+#include <gtest/gtest.h>
+
+#include "Board.h"
+
+TEST(PawnTest, SimpleMove) {
+    Board board;
+    Pawn blackPawn(3,0, Color::Black);
+    board.setPiece(3,0, blackPawn);
+
+    EXPECT_TRUE(blackPawn.canMove(4, 0, board));
+    EXPECT_FALSE(blackPawn.canMove(4, 1, board));
+
+    Pawn whitePawn(3, 1, Color::White);
+    board.setPiece(3, 1, whitePawn);
+
+    EXPECT_FALSE(whitePawn.canMove(4, 1, board));
+    EXPECT_TRUE(whitePawn.canMove(2, 1, board));
+}
+
+TEST(PawnTest, MoveFromStartPosition) {
+    Board board;
+    Pawn blackPawn(1,0, Color::Black);
+    board.setPiece(1,0, blackPawn);
+
+    EXPECT_TRUE(blackPawn.canMove(3, 0, board));
+    Move moveBlackPawn(1, 0, 3, 0);
+    board.makeMove(moveBlackPawn);
+
+    EXPECT_FALSE(blackPawn.canMove(5,0,board));
+
+    Pawn whitePawn(6,1, Color::White);
+    board.setPiece(6,1, whitePawn);
+
+    EXPECT_TRUE(whitePawn.canMove(4, 1, board));
+    Move moveWhitePawn(6, 1, 4, 1);
+    board.makeMove(moveWhitePawn);
+
+    EXPECT_FALSE(whitePawn.canMove(3,1,board));
+}
+
+TEST(Pawn, JumpFromStartPosition) {
+    Board board;
+    Pawn blackPawn(1,0, Color::Black);
+    board.setPiece(1,0, blackPawn);
+    board.setPiece(2, 0, Rook(2,0, Color::White));
+
+    EXPECT_FALSE(blackPawn.canMove(3, 0, board));
+
+    Pawn whitePawn(6,1, Color::White);
+    board.setPiece(6,1, whitePawn);
+    board.setPiece(5, 1, Rook(5,1, Color::White));
+
+    EXPECT_FALSE(whitePawn.canMove(4, 0, board));
+}
+
+TEST(PawnTest, Taking) {
+    Board board;
+    Pawn blackPawn(1,1, Color::Black);
+    board.setPiece(1,1, blackPawn);
+    board.setPiece(2,0, Pawn(2, 0, Color::White));
+    board.setPiece(2,2, Pawn(2, 2, Color::White));
+
+    EXPECT_TRUE(blackPawn.canMove(2, 1, board));
+    EXPECT_TRUE(blackPawn.canMove(2, 2, board));
+
+    Pawn whitePawn(6,1, Color::White);
+    board.setPiece(6,1, whitePawn);
+    board.setPiece(5,0, Pawn(5, 0, Color::White));
+    board.setPiece(5,2, Pawn(5, 2, Color::Black));
+
+    EXPECT_FALSE(whitePawn.canMove(5, 0, board));
+    EXPECT_TRUE(whitePawn.canMove(5, 2, board));
+}
